4d159d882063bb61a62e01d2d74c0bf4cd994382
Amilcar J. Infante <amilcar.infante@globant.com>
2014-08-07 17:45:09 -0300

    71854248 - Update node-visualforce-module with the new feature
    Added new feature to allows the user to declare/configure a list of
    dependent static resources names from the server being able to download
    them

--- README.md
+++ README.md
@@ -22,9 +22,9 @@ This object has two methods, called build() and execute() which allows the user 
 This method initialize the configuration of resources.
 
 #####Build Config Options.
-	- inputPath: defines the source folder structure.
-	- outputPath: defines the target forlder structure.
-	- staticResourceFolder: defines internal source structure where you put css, js, img and font folders.
+	- inputPath: defines the source folder structure (optional, by default => input/).
+	- outputPath: defines the target folder structure (optional, by default => output/).
+	- staticResourceFolder: defines internal source structure where you put css, js, img and font folders (optional, by default => staticresources/).
 
 #####Build Config Example.
 ```js
@@ -47,7 +47,7 @@ You have to put all your project files inside the following folder structure:
         - /img
         - /fonts
 ```
-If you have not create the structure the module will create the base structure for you.
+If you did not created the structure the module will create the base structure for you.
 
 Internally you need to reference the static resources (css, js, img, fonts) using relative ('/') paths, for example:
 - StyleSheets: "../staticresources/'static resources name'/css/sample.css"
@@ -55,7 +55,7 @@ Internally you need to reference the static resources (css, js, img, fonts) usin
 - Images: "../staticresources/'static resources name'/img/sample.png"
 - Fonts:  "../fonts/Roboto/Roboto-Regular.ttf" (in sample.css)
 
-if the user doesn't provide any options it takes the default values that are shown in the previous example.
+if the user doesn't provide any options it takes the default values.
 
 ####Build.execute(callback)
 This method executes the conversion of files and pack tasks with the static content provided by the user.
@@ -74,11 +74,19 @@ build.execute(function(args){
 ```
 
 ###Deploy Object.
-This object has two methods, called deploy() and execute() which allows the user to setup configuration, send the package and static files to the saleforce.con org.
+This object has three methods, called deploy(), execute() and destroy() which allows the user to setup configuration, send the package and static files to the saleforce.com org, and undeploy pages or staticresources from the salesforce.com org.
 
 ####Deploy.deploy(options).
-This method allows the user to setup the configuration for the deployment process to salesforce.com. It receives a config object
-and sets the org credentials and type of content that the user wants to upload.
+This method allows the user to setup the configuration for the deployment process to salesforce.com. It receives a config object and sets the org credentials and type of content that the user wants to upload.
+
+#####Deploy Config Options.
+  - user: Salesforce Username (required).
+  - pass: Salesforce Password (required).
+  - token: Salesforce Token (required).
+  - pkg: package to deploy. It is possible to deploy all the files from pages and staticresources using '*', or define which pages/staticresources deploy using a list separated by commas (required).
+  - serverUrl: server url (optional, by default => https://login.salesforce.com).
+  - apiVersion: Api Version (optional, by default => 29.0).
+  - outputPath: defines the target folder structure (optional, by default => output/).
 
 ####Deploy method usage example.
 ```js
@@ -87,8 +95,7 @@ var deploy = visual.deploy({
   user:'myusername@test.com',
   pass:      'mypassword',
   token:     'mytoken',
-  serverurl: 'https://test.salesforce.com', // default => https://login.salesforce.com
-  apiVersion: '29.0',
+  apiVersion: '30.0',
   // Target-specific file lists and/or options go here.
   pkg: {   // Package to deploy
     staticresource: ['*'],
@@ -96,7 +103,7 @@ var deploy = visual.deploy({
   }
 });
 ```
-if the user doesn't provide any options it takes the default values that are shown in the previous example.
+if the user doesn't provide any options it takes the default values.
 
 ####Deploy.execute().
 This method sends the specific content to saleforce.com.
@@ -115,4 +122,61 @@ In the case the user wants to undeploy all the site or some specific pages he ca
 var visual = require('node-visualforce-module');
 var deploy = visual.deploy(options);
 deploy.destroy();
+```
+
+###Retrieve Object.
+This object has two methods, called retrieve() and execute() which allows the user to setup configuration, retrieve the configured pages and staticresources from saleforce.com org.
+
+####Retrieve.retrieve(options).
+This method allows the user to setup the configuration to be used in the retrieve process to salesforce.com. It receives a config object and sets the org credentials and type of content that the user wants to retrieve.
+
+#####Retrieve Config Options.
+  - user: Salesforce Username (required).
+  - pass: Salesforce Password (required).
+  - token: Salesforce Token (required).
+  - pkg: package to deploy. It is possible to deploy all the files from pages and staticresources using '*', or define which pages/staticresources deploy using a list separated by commas (required).
+  - serverUrl: server url (optional, by default => https://login.salesforce.com).
+  - apiVersion: Api Version (optional, by default => 29.0).  
+  - inputPath: defines the parent of the folder to save the uncompressed staticresources (optional, by default => input/).
+  - outputPath: defines the target folder structure (optional, by default => output/).
+  - staticResourceFolder: defines staticresources folder where the uncompressed static resources will be saved (optional, by default => staticresources/).
+
+####Deploy method usage example.
+```js
+var visual = require('node-visualforce-module');
+var retrieve = visual.retrieve({
+  user:'myusername@test.com',
+  pass:      'mypassword',
+  token:     'mytoken',
+  apiVersion: '30.0',
+  //Target-specific file lists and/or options go here.
+  pkg: {   //Package to deploy
+    staticresource: ['*'],
+    pages: ['*']
+  }
+});
+```
+if the user doesn't provide any options it takes the default values.
+
+####Retrieve.execute().
+This method retrieves the specific content from saleforce.com.
+
+###Retrieve execute usage example.
+```js
+var visual = require('node-visualforce-module');
+var retrieve = visual.retrieve(options);
+deploy.execute();
+```
+
+###Proxy options
+If you are under a proxy you will need to add the proxy host and port to the '<b>deploy</b>' and '<b>retrieve</b>' config options so the ant server can reach the org:
+```js
+  options:{
+    ...
+    proxyConfig: {
+      proxyHost: "proxy.domain.com",
+      proxyPort: "1111"
+    }
+  }
+})
 ```
--- visualforce.js
+++ visualforce.js
@@ -2,6 +2,9 @@
 var grunt = require('grunt');
 var path = require('path');
 var utils = require('./lib/utils');
+var fs = require('fs');
+var antUtils = require('./lib/antUtils')(grunt);
+var unzipper = require('./lib/unzipper');
 var page = require('./lib/page.js');
 var staticResources = require('./lib/staticResources.js');
 var conf = require('./lib/configuration').getConfiguration();
@@ -21,6 +24,13 @@ exports.build = function(options) {
   var configOptions = {};
 
   var init = function(options) {
+
+    configOptions = {
+      inputPath: conf.path.inputPath,
+      outputPath: conf.path.outputPath,
+      staticResourceFolder: conf.path.staticResourceFolder
+    };
+
     //verifies the option object content
     if (utils.size(options) > 0) {
 
@@ -37,13 +47,7 @@ exports.build = function(options) {
         configOptions.staticResourceFolder = options.staticResourceFolder;
       }
 
-    } else {
-      //sets default options to config object
-      configOptions.inputPath = conf.path.inputPath;
-      configOptions.outputPath = conf.path.outputPath;
-      configOptions.staticResourceFolder = conf.path.staticResourceFolder;
-      configOptions.staticResourceName = conf.fileNames.staticResourceName;
-    }
+    } 
   };
 
   //executes the contructor method
@@ -91,166 +95,14 @@ exports.deploy = function(options) {
 
   var configOptions = {};
   var template = false;
-
-  /**
-   * function that seeks for the metadata
-   * @param  {string} key key definition
-   * @return {void}
-   */
-  var lookupMetadata = function(key) {
-    key = key.toLowerCase();
-    var typeName;
-    // try to match on metadata type
-    if (metadata[key] && metadata[key].xmlType) {
-      typeName = metadata[key].xmlType;
-    } else {
-      // try to match on folder
-      Object.keys(metadata).forEach(function(mk) {
-        var folder = metadata[mk].folder;
-        if (typeof folder === 'string' && folder.toLowerCase() === key) {
-          typeName = metadata[mk].xmlType;
-        } else if (key === 'documents') {
-          typeName = metadata.document.xmlType;
-        } else if (key === 'emails') {
-          typeName = metadata.email.xmlType;
-        } else if (key === 'reports') {
-          typeName = metadata.report.xmlType;
-        } else if (key === 'dashboards') {
-          typeName = metadata.dashboard.xmlType;
-        }
-      });
-    }
-    return typeName;
-  };
-
-  /**
-   * function that clears the deployment folder structure.
-   * @return {void}
-   */
-  var clearLocalTmp = function() {
-    if (grunt.file.exists(localTmp)) {
-      grunt.file.delete(localTmp, {
-        force: true
-      });
-    }
-  };
-
-  /**
-   * function that creates the directories for deployment
-   * @return {void}
-   */
-  var makeLocalTmp = function() {
-    clearLocalTmp();
-    grunt.file.mkdir(localTmp);
-    grunt.file.mkdir(localTmp + '/ant');
-  };
-
-  /**
-   * function that pase if the auth params comes from command line and set them
-   * on options object
-   * @param  {object} options configuration object
-   * @param  {string} target  deployment org
-   * @return {void}
-   */
-  var parseAuth = function(options, target) {
-    var un = options.user;
-    var pw = options.pass;
-    var tk = options.token;
-
-    if (tk) {
-      pw += tk;
-    }
-    if (!un) {
-      grunt.log.error('no username specified for ' + target);
-    }
-    if (!pw) {
-      grunt.log.error('no password specified for ' + target);
-    }
-    if (!un || !pw) {
-      grunt.fail.warn('username/password error');
-    }
-
-    options.user = un;
-    options.pass = pw;
-
-    grunt.log.writeln('User -> ' + options.user.green);
-  };
-
-  /**
-   * function that runs ant task
-   * @param  {string}   task   ant task label
-   * @param  {string}   target notification text
-   * @param  {Function} done   async function
-   * @return {void}
-   */
-  var runAnt = function(task, target, done) {
-    var args = [
-      '-buildfile',
-      localTmp + '/ant/build.xml',
-      '-lib',
-      localLib,
-      '-Dbasedir=' + process.cwd()
-    ];
-    args.push(task);
-    grunt.log.debug('ANT CMD: ant ' + args.join(' '));
-    grunt.log.writeln('Starting ANT ' + task + '...');
-    var ant = grunt.util.spawn({
-      cmd: 'ant',
-      args: args
-    }, function(error, result, code) {
-      if (error) {
-        grunt.fail.warn(error, code);
-      } else {
-        grunt.log.ok(task + ' target ' + target + ' successful');
-      }
-      done(error, result);
-    });
-    ant.stdout.on('data', function(data) {
-      grunt.log.write(data);
-    });
-    ant.stderr.on('data', function(data) {
-      grunt.log.error(data);
-    });
-  };
-
-  /**
-   * function that build the package xml to deploy resources
-   * @param  {object} pkg     meta-data objecte
-   * @param  {string} version saleforce api version
-   * @return {array}          array whith xml
-   */
-  var buildPackageXml = function(pkg, version) {
-    var packageXml = [
-      '<?xml version="1.0" encoding="UTF-8"?>',
-      '<Package xmlns="http://soap.sforce.com/2006/04/metadata">'
-    ];
-    if (pkg) {
-      Object.keys(pkg).forEach(function(key) {
-        var type = pkg[key];
-        var typeName = lookupMetadata(key);
-        if (!typeName) {
-          grunt.fail.fatal(key + ' is not a valid metadata type');
-        }
-        packageXml.push('    <types>');
-        type.forEach(function(t) {
-          packageXml.push('        <members>' + t + '</members>');
-        });
-        packageXml.push('        <name>' + typeName + '</name>');
-        packageXml.push('    </types>');
-      });
-    }
-    packageXml.push('    <version>' + version + '</version>');
-    packageXml.push('</Package>');
-    return packageXml.join('\n');
-  };
-
+  
   /**
    * constructor method
-   * @param  {object} options conguration object
+   * @param  {object} options configuration object
    * @return {void}
    */
   var init = function(options) {
-    makeLocalTmp();
+    antUtils.makeLocalTmp();
 
     configOptions = {
       target: 'default',
@@ -259,7 +111,7 @@ exports.deploy = function(options) {
       token: false,
       root: conf.path.outputPath,
       apiVersion: conf.page.options.apiVersion,
-      serverurl: 'https://login.salesforce.com',
+      serverurl: conf.serverUrl,
       pollWaitMillis: 10000,
       maxPoll: 20,
       checkOnly: false,
@@ -301,6 +153,10 @@ exports.deploy = function(options) {
         configOptions.serverurl = options.serverurl;
       }
 
+      if ((typeof options.outputPath) == 'string' && options.outputPath.length > 0) {
+        configOptions.root = options.outputPath;
+      }
+
     } else {
       template = grunt.file.read(localAnt + '/antdeploy.build.xml');
     }
@@ -331,20 +187,20 @@ exports.deploy = function(options) {
     execute: function() {
 
       grunt.log.writeln('Deploy Target -> ' + this.getConfig().target);
-      parseAuth(this.getConfig(), this.getConfig().target);
+      antUtils.parseAuth(this.getConfig(), this.getConfig().target);
 
       this.getConfig().tests = [];
       var buildFile = grunt.template.process(template, {
         data: this.getConfig()
       });
-
+     
       grunt.file.write(localTmp + '/ant/build.xml', buildFile);
-
-      var packageXml = buildPackageXml(this.getConfig().pkg, this.getConfig().apiVersion);
+ 
+      var packageXml = antUtils.buildPackageXml(this.getConfig().pkg, this.getConfig().apiVersion);
       grunt.file.write(this.getConfig().root + '/package.xml', packageXml);
 
-      runAnt('deploy', this.getConfig().target, function(err, result) {
-        clearLocalTmp();
+      antUtils.runAnt('deploy', this.getConfig().target, function(err, result) {
+        antUtils.clearLocalTmp();
       });
     },
     /**
@@ -352,12 +208,12 @@ exports.deploy = function(options) {
      * @return {void}
      */
     destroy: function() {
-      makeLocalTmp();
+      antUtils.makeLocalTmp();
 
       var template = grunt.file.read(localAnt + '/antdeploy.build.xml');
 
       grunt.log.writeln('Destroy Target -> ' + this.getConfig().target);
-      parseAuth(this.getConfig(), this.getConfig().target);
+      antUtils.parseAuth(this.getConfig(), this.getConfig().target);
 
       this.getConfig().tests = [];
       var buildFile = grunt.template.process(template, {
@@ -365,14 +221,191 @@ exports.deploy = function(options) {
       });
       grunt.file.write(localTmp + '/ant/build.xml', buildFile);
 
-      var packageXml = buildPackageXml(null, this.getConfig().apiVersion);
-      grunt.file.write(his.getConfig().root + '/package.xml', packageXml);
+      var packageXml = antUtils.buildPackageXml(null, this.getConfig().apiVersion);
+      grunt.file.write(this.getConfig().root + '/package.xml', packageXml);
 
-      var destructiveXml = buildPackageXml(this.getConfig().pkg, this.getConfig().apiVersion);
-      grunt.file.write(his.getConfig().root + '/destructiveChanges.xml', destructiveXml);
+      var destructiveXml = antUtils.buildPackageXml(this.getConfig().pkg, this.getConfig().apiVersion);
+      grunt.file.write(this.getConfig().root + '/destructiveChanges.xml', destructiveXml);
 
-      runAnt('deploy', target, function(err, result) {
-        clearLocalTmp();
+      antUtils.runAnt('deploy', this.getConfig().target, function(err, result) {
+        antUtils.clearLocalTmp();
+      });
+    }    
+  };
+};
+
+/**
+ * class that represents the retrieve process
+ * @param  {object} options configuration object
+ * @return {void}
+ */
+exports.retrieve = function(options) {
+
+  var configOptions = {};
+  var template = false;
+
+  /**
+   * constructor method
+   * @param  {object} options configuration object
+   * @return {void}
+   */
+  var init = function(options) {
+    antUtils.makeLocalTmp();
+
+    configOptions = {
+      target: 'default',
+      user: false,
+      pass: false,
+      token: false,
+      root: localTmp,
+      apiVersion: conf.page.options.apiVersion,
+      serverurl: conf.serverUrl,
+      pollWaitMillis: 10000,
+      maxPoll: 20,
+      retrieveTarget: false,
+      unzip: true,
+      existingPackage: false,
+      inputPath: conf.path.inputPath,
+      outputPath: conf.path.outputPath,
+      staticResourceFolder: conf.path.staticResourceFolder
+    };
+
+    if (utils.size(options) > 0) {
+
+      if (options.hasOwnProperty('proxyConfig')) {
+        configOptions.proxyConfig = options.proxyConfig;
+        template = grunt.file.read(localAnt + '/antretrieve.build.proxy.xml');
+      } else {
+        template = grunt.file.read(localAnt + '/antretrieve.build.xml');
+      }
+
+      if ((typeof options.target) == 'string' && options.target.length > 0) {
+        configOptions.target = options.target;
+      }
+
+      if ((typeof options.user) == 'string' && options.user.length > 0) {
+        configOptions.user = options.user;
+      }else{
+        grunt.log.writeln(String('Username not defined').red);
+      }
+
+      if ((typeof options.pass) == 'string' && options.pass.length > 0) {
+        configOptions.pass = options.pass;
+      }else{
+        grunt.log.writeln(String('Password not defined').red);
+      }
+
+      if ((typeof options.token) == 'string' && options.token.length > 0) {
+        configOptions.pass += options.token;
+      }else{
+        grunt.log.writeln(String('Token not defined').red);
+      }
+
+      if ((typeof options.apiVersion) == 'string' && options.apiVersion.length > 0) {
+        configOptions.apiVersion = options.apiVersion;
+      }
+
+      if ((typeof options.serverurl) == 'string' && options.serverurl.length > 0) {
+        configOptions.serverurl = options.serverurl;
+      }
+      
+      if ((typeof options.inputPath) == 'string' && options.inputPath.length > 0) {
+        configOptions.inputPath = options.inputPath;
+      }
+
+      if ((typeof options.outputPath) == 'string' && options.outputPath.length > 0) {
+        configOptions.outputPath = options.outputPath;
+      }
+ 
+      if ((typeof options.staticResourceFolder) == 'string' && options.staticResourceFolder.length > 0) {
+        configOptions.staticResourceFolder = options.staticResourceFolder;
+      }    
+
+    } else {
+      template = grunt.file.read(localAnt + '/antretrieve.build.xml');
+    }
+
+    if (utils.size(options.pkg) > 0) {
+      configOptions.pkg = options.pkg;
+    } else {
+      throw 'pkg attribute is not defined';
+    }
+
+    configOptions.outputpages = path.join(configOptions.outputPath, conf.path.pagesFolder);
+  };
+
+  //executes contructor method
+  init(options);
+
+  return {
+    //Unzip callback
+    retrieveComplete: function() {
+      grunt.log.writeln('Retrieve Complete');
+    },
+
+    execute: function() {
+
+      grunt.log.writeln('This operation could take some seconds or minutes, depending on the size of the static resources to retrieve from the org');
+        
+      var done = this.retrieveComplete,
+          target = configOptions.target,
+          template;
+
+      template = grunt.file.read(localAnt + '/antretrieve.build.xml');
+      var staticResourceFolder = path.join(configOptions.staticResourceFolder, path.sep);
+      var inputPath = path.join(configOptions.inputPath, staticResourceFolder);
+
+      grunt.log.writeln('Retrieve Target -> ' + target);
+      antUtils.parseAuth(options, target);
+      configOptions.root = path.normalize(configOptions.root);
+      configOptions.unpackaged = path.join(localTmp,'/package.xml');
+      
+      if(!configOptions.retrieveTarget) {configOptions.retrieveTarget = configOptions.root;}
+      
+      var buildFile = grunt.template.process(template, { data: configOptions });
+      grunt.file.write(path.join(localTmp,'/ant/build.xml'), buildFile);
+     
+      if (!configOptions.existingPackage) {
+        var packageXml = antUtils.buildPackageXml(configOptions.pkg, configOptions.apiVersion);
+        grunt.file.write(path.join(configOptions.root,'/package.xml'), packageXml);
+        console.log('package created -> ' +  grunt.file.exists(configOptions.root,'/package.xml'));       
+      } else {
+        if(grunt.file.exists(configOptions.root,'/package.xml')){
+          grunt.file.copy(path.join(configOptions.root,'/package.xml'), path.join(localTmp,'/package.xml'));
+        } else {
+          grunt.log.error('No Package.xml file found in ' + configOptions.root);
+          console.log('No Package.xml file found in ' + configOptions.root);
+        }
+      }
+
+      antUtils.runAnt('retrieve', target, function(err, result) {
+        var tmpStaticResources = path.join(configOptions.root, '/staticresources/');
+        var tmpPages = path.join(configOptions.root, '/pages/');
+        var outputPagesFolder = configOptions.outputpages;
+
+        //Copy pages to outputPath
+        if(grunt.file.exists(tmpPages)){
+          grunt.file.recurse(tmpPages, function(abspath, rootdir, subdir, filename) { 
+            grunt.file.copy(abspath, path.join(outputPagesFolder, filename));      
+          });
+        }
+
+        //Unzip staticresources on input/staticresources
+        if(grunt.file.exists(tmpStaticResources)){            
+
+          var staticResources = fs.readdirSync(tmpStaticResources);
+
+          if( staticResources !== null & staticResources.length > 0 ) {
+
+            //Filter static Resources discarding the meta xmls
+            staticResources = unzipper.filterStaticResources(staticResources);
+
+            //Decompress all the static resources retrieved from the org
+            unzipper.unzipStaticResources(tmpStaticResources, inputPath, staticResources, done);
+          }else{
+            console.log('There are not static resources retrieved from the org')
+          }
+        }
       });
     }
   };
